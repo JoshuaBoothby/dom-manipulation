eaa9034a8e25c57ea3bc12eeda9c00213d66d6da
Joshua Boothby <Mona.Lisa@example.com>
2025-02-19 18:58:11 -0600

    removed the json html and styled the h2 with js

--- domManipulation.js
+++ domManipulation.js
@@ -3,3 +3,6 @@
 const textChange = document.getElementById("domP");
 
 textChange.innerHTML = "Big text change!";
+
+const h2Style = document.querySelector("h2");
+h2Style.style.fontStyle = "italic";
