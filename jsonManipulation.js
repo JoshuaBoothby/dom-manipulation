3055112b635b1ea955489de585710d8155c832e0
Joshua Boothby <Mona.Lisa@example.com>
2025-02-19 21:03:05 -0600

    finished part 3

--- jsonManipulation.js
+++ jsonManipulation.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const edgeRunner = {
+  handle: "Railgun",
+  age: "38",
+  role: "Solo",
+  rank: "4",
+  skills: ["Combat", "Stealth", "Athletics"],
+  cyberware: ["Sandevistan", "Cyberoptics"],
+  weapons: ["Pistol", "Sniper Rifle"],
+};
+
+const edgeRunnerJSON = JSON.stringify(edgeRunner);
+
+console.log(edgeRunnerJSON);
+
+const edgeRunnerParsed = JSON.parse(edgeRunnerJSON);
+
+console.log(edgeRunnerParsed);
