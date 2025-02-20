15f8d558c4b30794ebf71bee10ebe63a795b138a
Joshua Boothby <Mona.Lisa@example.com>
2025-02-19 20:50:59 -0600

    finished part 2

--- domManipulation.js
+++ domManipulation.js
@@ -4,5 +4,18 @@ const textChange = document.getElementById("domP");
 
 textChange.innerHTML = "Big text change!";
 
-const h2Style = document.querySelector("h2");
+const h2Style = document.querySelector("#domHead");
+
 h2Style.style.fontStyle = "italic";
+
+const domBtn = document.getElementById("domBtn");
+
+domBtn.addEventListener("click", function (event) {
+  alert("Button clicked!");
+});
+
+const domA = document.querySelector("a");
+domA.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Link clicked!");
+});
